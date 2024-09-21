d36c1183f332e0bb811047766fb98c3a3e1c4f73
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-09-21 22:22:10 +0500

    Create Account.cs

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CheckDrive.Mobile.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Passport { get; set; }
+        public string Address { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
