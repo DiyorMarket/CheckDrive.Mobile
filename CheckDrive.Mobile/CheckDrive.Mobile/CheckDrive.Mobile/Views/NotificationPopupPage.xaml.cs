e8659a1b5786e71410eeef32ad7201f33b8e9952
Azamat Giyosov <azamatgiasov04@gmail.com>
2024-09-27 12:48:12 +0500

    Create Notification Popup page

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/NotificationPopupPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CheckDrive.Mobile.ViewModels;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheckDrive.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotificationPopupPage : PopupPage
+    {
+        private readonly ChatViewModel _chatViewModel;
+        public NotificationPopupPage(string massage)
+        {
+            InitializeComponent();
+
+            _chatViewModel = new ChatViewModel();
+            _chatViewModel.Message = massage; 
+            
+            BindingContext = _chatViewModel;
+        }
+
+        private async void PopupPage_BackgroundClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
