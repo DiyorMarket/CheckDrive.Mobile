7dbe7b0da6a5d1e768c1ef23d529e71e668065a7
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-09-28 17:32:23 +0500

    Remove empty line

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/NotificationPopupPage.xaml.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/NotificationPopupPage.xaml.cs
@@ -1,12 +1,6 @@
 ﻿using CheckDrive.Mobile.ViewModels;
 using Rg.Plugins.Popup.Pages;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CheckDrive.Mobile.Views
