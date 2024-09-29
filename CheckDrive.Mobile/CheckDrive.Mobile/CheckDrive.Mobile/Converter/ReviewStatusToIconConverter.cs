630e56629a90f5fb565b7e62990e9685aa566757
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-09-29 12:16:28 +0500

    implement progress bar manually

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Converter/ReviewStatusToIconConverter.cs
@@ -0,0 +1,38 @@
+﻿using CheckDrive.Mobile.Models.Enums;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CheckDrive.Mobile.Converter
+{
+    public class ReviewStatusToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ReviewStatus status)
+            {
+                switch (status)
+                {
+                    case ReviewStatus.InProgress:
+                        return "";
+                    case ReviewStatus.Approved:
+                        return "icon_success.png";
+                    case ReviewStatus.RejectedByReviewer:
+                    case ReviewStatus.RejectedByDriver:
+                        return "icon_error.png";
+                    case ReviewStatus.NotStarted:
+                        return "icon_circle.png";
+                    default:
+                        return "icon_circle.png";
+                }
+            }
+
+            return "icon_circle.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Converter/ReviewTypeToShowLineBelowConverter.cs
@@ -0,0 +1,31 @@
+﻿using CheckDrive.Mobile.Models.Enums;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CheckDrive.Mobile.Converter
+{
+    public class ReviewTypeToShowLineBelowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ReviewType type)
+            {
+                switch (type)
+                {
+                    case ReviewType.DispatcherReview:
+                        return false;
+                    default:
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
