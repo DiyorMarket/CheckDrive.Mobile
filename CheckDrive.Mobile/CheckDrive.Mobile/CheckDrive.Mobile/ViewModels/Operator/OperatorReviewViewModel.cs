b91b82b2d5205fbdc21ff0125ca3e72177a0e3a2
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-10-06 23:34:34 +0500

    add operator pages

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/ViewModels/Operator/OperatorReviewViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CheckDrive.Mobile.Models;
+using System.Collections.Generic;
+
+namespace CheckDrive.Mobile.ViewModels.Operator
+{
+    public class OperatorReviewViewModel : BaseViewModel
+    {
+        public string FullName { get; }
+        public List<OilMark> OilMarks { get; set; }
+        public string Comments { get; set; }
+
+        public OperatorReviewViewModel(AccountDto account, List<OilMark> oilMarks)
+        {
+            FullName = account.FirstName + " " + account.LastName;
+            OilMarks = oilMarks;
+        }
+    }
+}
