a4e5e9c23ce39c8013ac015de8ec5fc848307777
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-10-08 13:37:33 +0500

    add error & success popups

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/Popup/SuccessPopup.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace CheckDrive.Mobile.Views.Popup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SuccessPopup : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public string Message { get; }
+
+        public SuccessPopup()
+        {
+
+        }
+
+        public SuccessPopup(string message)
+        {
+            InitializeComponent();
+
+            Message = message;
+
+            BindingContext = this;
+        }
+
+        private async void Close_Clicked(object sender, System.EventArgs e)
+        {
+            await Rg.Plugins.Popup.Services.PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
