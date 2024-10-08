ead0dc546e26d21c813c0ba7927cbd9320fff25d
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-10-08 13:39:54 +0500

    Create BooleanToReviewStatusIconConverter.cs

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Converter/BooleanToReviewStatusIconConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CheckDrive.Mobile.Converter
+{
+    public class BooleanToReviewStatusIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isApproved)
+            {
+                return isApproved ? "icon_success.png" : "icon_error.png";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
