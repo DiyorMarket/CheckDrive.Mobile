be36bb18f753772d6b986eb342f5c3a2c18f52fe
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-10-09 15:53:35 +0500

    update history view model to handle filtration

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/ViewModels/Doctor/DoctorHistoryViewModel.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/ViewModels/Doctor/DoctorHistoryViewModel.cs
@@ -7,6 +7,8 @@ using CheckDrive.Mobile.Stores.Doctor;
 using Xamarin.Forms;
 using System.Linq;
 using System.Collections.Generic;
+using CheckDrive.Mobile.Views.Doctor;
+using CheckDrive.Mobile.Models.Enums;
 
 namespace CheckDrive.Mobile.ViewModels.Doctor
 {
@@ -19,6 +21,14 @@ namespace CheckDrive.Mobile.ViewModels.Doctor
 
         public Command RefreshCommand { get; }
         public Command<string> SearchCommand { get; }
+        public Command FilterCommand { get; }
+
+        private DoctorFilter _filters = null;
+        public DoctorFilter Filters
+        {
+            get => _filters;
+            set => SetProperty(ref _filters, value);
+        }
 
         public DoctorHistoryViewModel()
         {
@@ -29,6 +39,7 @@ namespace CheckDrive.Mobile.ViewModels.Doctor
 
             RefreshCommand = new Command(async () => await LoadHistoryAsync());
             SearchCommand = new Command<string>(OnSearch);
+            FilterCommand = new Command(async () => await OnFilterAsync());
         }
 
         public async Task LoadHistoryAsync()
@@ -70,6 +81,59 @@ namespace CheckDrive.Mobile.ViewModels.Doctor
                 ? _histories
                 : _histories.Where(x => x.DriverName.ToLower().Contains(searchText.ToLower())).ToList();
 
+            UpdateHistories(histories);
+        }
+
+        private async Task OnFilterAsync()
+        {
+            var completionSource = new TaskCompletionSource<DoctorFilter>();
+            var popup = new DoctorFiltersPopup
+            {
+                BindingContext = new DoctorFiltersViewModel(_histories, completionSource, Filters)
+            };
+
+            try
+            {
+                await Rg.Plugins.Popup.Services.PopupNavigation.Instance.PushAsync(popup, true);
+
+                var result = await completionSource.Task;
+                await Rg.Plugins.Popup.Services.PopupNavigation.Instance.PopAsync();
+
+                if (result != null)
+                {
+                    Filters = result;
+                    ApplyFilters(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayErrorAsync("Saralash modalni ochishda muammo ro'y berdi.", ex.Message);
+            }
+        }
+
+        private void ApplyFilters(DoctorFilter filters)
+        {
+            var query = _histories.AsQueryable();
+
+            if (filters.SelectedDriverId.HasValue)
+            {
+                query = query.Where(x => x.DriverId == filters.SelectedDriverId.Value);
+            }
+
+            if (filters.SelectedStatus.HasValue)
+            {
+                query = query.Where(x => filters.SelectedStatus.Value == ReviewStatus.Approved ? x.IsApproved : !x.IsApproved);
+            }
+
+            query = query.Where(x => x.Date.Date >= filters.StartDate.Date);
+            query = query.Where(x => x.Date.Date <= filters.EndDate.Date);
+            query = SortHistory(query, filters.SortBy);
+
+            UpdateHistories(query);
+        }
+
+        private void UpdateHistories(IEnumerable<DoctorHistory> histories)
+        {
             var groupedHistory = histories
                         .OrderByDescending(x => x.Date)
                         .GroupBy(d => d.Date.Date)
@@ -80,6 +144,21 @@ namespace CheckDrive.Mobile.ViewModels.Doctor
             foreach (var history in groupedHistory)
             {
                 Histories.Add(history);
+            }
+        }
+
+        private static IQueryable<DoctorHistory> SortHistory(IQueryable<DoctorHistory> query, string sortBy)
+        {
+            switch (sortBy.ToLower().Trim())
+            {
+                case "date_asc":
+                    return query.OrderBy(x => x.Date);
+                case "name_asc":
+                    return query.OrderBy(x => x.DriverName);
+                case "name_desc":
+                    return query.OrderByDescending(x => x.DriverName);
+                default:
+                    return query.OrderByDescending(x => x.Date);
             }
         }
     }
