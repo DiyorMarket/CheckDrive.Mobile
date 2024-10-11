5718f6b4ec0d52258fd87cc467129b3437bd5bb1
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-10-11 21:40:09 +0500

    fix error handling navigation

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/App.xaml.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/App.xaml.cs
@@ -134,7 +134,7 @@ namespace CheckDrive.Mobile
                     await Shell.Current.GoToAsync(nameof(LoginPage), true);
                 }
 
-                await Shell.Current.GoToAsync(nameof(UnknownErrorPage), true);
+                await Shell.Current.Navigation.PushAsync(new UnknownErrorPage(), true);
             });
         }
     }
