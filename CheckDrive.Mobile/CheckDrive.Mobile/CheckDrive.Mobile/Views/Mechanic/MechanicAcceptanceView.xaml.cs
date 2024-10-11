31d583a6cd89643919ff44cd79a8fabd9e87e955
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-10-11 07:19:01 +0500

    implement mechanic handover views

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/Mechanic/MechanicAcceptanceView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheckDrive.Mobile.Views.Mechanic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MechanicAcceptanceView : ContentView
+    {
+        public MechanicAcceptanceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/Mechanic/MechanicHomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheckDrive.Mobile.Views.Mechanic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MechanicHomePage : ContentPage
+    {
+        public MechanicHomePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            await HandoverView.LoadViewModelData();
+            base.OnAppearing();
+        }
+    }
+}
