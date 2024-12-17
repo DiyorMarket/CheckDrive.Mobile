dff6095587e33b74a4671cf97272e09559c6549a
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-17 22:02:29 +0500

    update dispatcher models

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Dispatcher/DispatcherReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CheckDrive.Mobile.Models.Dispatcher
+{
+    public class DispatcherReview
+    {
+        public int CheckPointId { get; set; }
+        public int DispatcherId { get; set; }
+        public string DispatcherName { get; set; }
+        public string Notes { get; set; }
+        public DateTime Date { get; set; }
+        public int FinalMileage { get; set; }
+        public decimal FuelConsumptionAmount { get; set; }
+        public decimal RemainingFuelAmount { get; set; }
+    }
+}
