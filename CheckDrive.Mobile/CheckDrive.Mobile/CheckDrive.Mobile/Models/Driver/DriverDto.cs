22e1c3b50d773467380401a9fbdf9f4685864644
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-17 22:02:54 +0500

    update driver models

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Driver/DriverDto.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Driver/DriverDto.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string FullName { get; set; }
-        public int? CheckPointId { get; set; }
+        public int? AssignedCarId { get; set; }
 
         public DriverDto()
         {
--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Driver/ReviewConfirmationRequest.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Models/Driver/ReviewConfirmationRequest.cs
@@ -4,8 +4,17 @@ namespace CheckDrive.Mobile.Models.Driver
 {
     public class ReviewConfirmationRequest
     {
-        public int CheckPointId { get; set; }
-        public ReviewType ReviewType { get; set; }
-        public string Message { get; set; }
+        public int CheckPointId { get; }
+        public ReviewType ReviewType { get; }
+        public bool IsAccepted { get; }
+        public string Notes { get; }
+
+        public ReviewConfirmationRequest(int checkPointId, ReviewType reviewType, bool isAcceptedByDriver, string notes)
+        {
+            CheckPointId = checkPointId;
+            ReviewType = reviewType;
+            IsAccepted = isAcceptedByDriver;
+            Notes = notes;
+        }
     }
 }
