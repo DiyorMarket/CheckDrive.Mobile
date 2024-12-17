4edf2a894a6c457817251129e91dce55059d3a1b
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-17 22:04:26 +0500

    update doctor store

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Stores/Doctor/DoctorStore.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Stores/Doctor/DoctorStore.cs
@@ -1,6 +1,5 @@
 ﻿using CheckDrive.Mobile.Models.Doctor;
 using CheckDrive.Mobile.Models.Driver;
-using CheckDrive.Mobile.Models.Review;
 using CheckDrive.Mobile.Services;
 using CheckDrive.Mobile.Stores.Account;
 using System.Collections.Generic;
@@ -20,12 +19,6 @@ namespace CheckDrive.Mobile.Stores.Doctor
             _accountStore = DependencyService.Get<IAccountStore>();
         }
 
-        public async Task CreateAsync(DoctorReview review)
-        {
-            var response = await _client.PostAsync($"reviews/doctors/{review.ReviewerId}", review);
-            response.EnsureSuccessStatusCode();
-        }
-
         public async Task<List<DriverDto>> GetDriversAsync()
         {
             var drivers = await _client.GetAsync<List<DriverDto>>("drivers");
@@ -33,12 +26,19 @@ namespace CheckDrive.Mobile.Stores.Doctor
             return drivers;
         }
 
-        public async Task<List<DoctorHistory>> GetReviewHistoryAsync(bool forceRefresh = false)
+        public async Task<List<DoctorReview>> GetReviews(bool forceRefresh = false)
         {
             var doctorId = await _accountStore.GetUserIdAsync();
-            var histories = await _client.GetAsync<List<DoctorHistory>>($"reviews/histories/doctors/{doctorId}");
+            var reviews = await _client.GetAsync<List<DoctorReview>>($"reviews/histories/doctors/{doctorId}");
 
-            return histories;
+            return reviews;
+        }
+
+        public async Task CreateAsync(DoctorReviewRequest request)
+        {
+            var response = await _client.PostAsync($"reviews/doctors/{request.DoctorId}", request);
+
+            response.EnsureSuccessStatusCode();
         }
     }
 }
