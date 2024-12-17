fb044557c8dbfd74b38abc33af91f6f3636ea6b2
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-17 22:04:34 +0500

    update dispatcher store

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Stores/Dispatcher/DispatcherStore.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Stores/Dispatcher/DispatcherStore.cs
@@ -1,5 +1,7 @@
-﻿using CheckDrive.Mobile.Models.Review;
+﻿using CheckDrive.Mobile.Models.Dispatcher;
 using CheckDrive.Mobile.Services;
+using CheckDrive.Mobile.Stores.Account;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -7,17 +9,27 @@ namespace CheckDrive.Mobile.Stores.Dispatcher
 {
     public class DispatcherStore : IDispatcherStore
     {
+        private readonly IAccountStore _accountStore;
         private readonly ApiClient _client;
 
         public DispatcherStore()
         {
             _client = DependencyService.Get<ApiClient>();
+            _accountStore = DependencyService.Get<IAccountStore>();
         }
 
-        public async Task CreateReveiwAsync(DispatcherReview review)
+        public async Task CreateReveiwAsync(DispatcherReviewRequest request)
         {
-            var response = await _client.PostAsync($"reviews/dispatchers/{review.ReviewerId}", review);
+            var response = await _client.PostAsync($"reviews/dispatchers/{request.DispatcherId}", request);
             response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<List<DispatcherHistory>> GetHistoryAsync()
+        {
+            var dispatcherId = await _accountStore.GetUserIdAsync();
+            var histories = await _client.GetAsync<List<DispatcherHistory>>($"reviews/histories/dispatchers/{dispatcherId}");
+
+            return histories;
         }
     }
 }
--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Stores/Dispatcher/IDispatcherStore.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Stores/Dispatcher/IDispatcherStore.cs
@@ -1,10 +1,12 @@
-﻿using CheckDrive.Mobile.Models.Review;
+﻿using CheckDrive.Mobile.Models.Dispatcher;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CheckDrive.Mobile.Stores.Dispatcher
 {
     public interface IDispatcherStore
     {
-        Task CreateReveiwAsync(DispatcherReview review);
+        Task CreateReveiwAsync(DispatcherReviewRequest request);
+        Task<List<DispatcherHistory>> GetHistoryAsync();
     }
 }
