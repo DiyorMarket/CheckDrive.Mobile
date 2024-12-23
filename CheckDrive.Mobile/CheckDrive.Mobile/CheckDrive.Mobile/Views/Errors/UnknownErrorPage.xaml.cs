c24233c1c428c1df459002918e614589a91eb73b
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-23 22:49:15 +0500

    improve styling

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/Errors/UnknownErrorPage.xaml.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Views/Errors/UnknownErrorPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,9 +13,29 @@ namespace CheckDrive.Mobile.Views.Errors
             InitializeComponent();
         }
 
+        public UnknownErrorPage(string message)
+        {
+            ErrorDetails.Text = message;
+        }
+
         private async void Close_Clicked(object sender, EventArgs e)
         {
             await Shell.Current.Navigation.PopAsync();
+        }
+
+        private void Expander_Tapped(object sender, System.EventArgs e)
+        {
+            if (sender is Expander expander)
+            {
+                if (expander.IsExpanded)
+                {
+                    ExpanderIcon.Source = "arrow_up.png";
+                }
+                else
+                {
+                    ExpanderIcon.Source = "arrow_down.png";
+                }
+            }
         }
     }
 }
