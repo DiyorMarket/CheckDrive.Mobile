8b9d4113d46d8c35acb0bf3f5186523897bff609
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-23 22:46:15 +0500

    add check point status converter

---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile/Converter/CheckPointStatusToIconConverter.cs
@@ -0,0 +1,30 @@
+﻿using CheckDrive.Mobile.Models.Enums;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CheckDrive.Mobile.Converter
+{
+    public class CheckPointStatusToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is CheckPointStatus status)
+            {
+                if (status == CheckPointStatus.Completed)
+                {
+                    return "icon_success.png";
+                }
+
+                return status == CheckPointStatus.Interrupted ? "icon_warning.png" : "icon_error.png";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
