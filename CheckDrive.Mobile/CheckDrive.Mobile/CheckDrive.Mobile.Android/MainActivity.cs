284fe3d2e1ec01db2c5bad2aa1cfccba77876674
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-24 22:34:06 +0500

    update splash screen

--- CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile.Android/MainActivity.cs
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 
 namespace CheckDrive.Mobile.Droid
 {
-    [Activity(Label = "ATP Garaj", Icon = "@drawable/logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "ATP Garaj", Icon = "@drawable/logo", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ CheckDrive.Mobile/CheckDrive.Mobile/CheckDrive.Mobile.Android/SplashActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace CheckDrive.Mobile.Droid
+{
+    [Activity(Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+        }
+    }
+}
